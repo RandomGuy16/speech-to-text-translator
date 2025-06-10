e4d1976031912e0e89e962ad2b60d8bd60f7d0c1
Cleofé <100363075+RandomGuy16@users.noreply.github.com>
2025-06-10 09:41:10 -0500

    Update main.py

--- whisper_test.py
+++ whisper_test.py
@@ -1,7 +1,6 @@
 import speech_recognition as sr
 import whisper
 
-# Grabas con speech_recognition
 #recognizer = sr.Recognizer()
 #with sr.Microphone() as source:
 #    recognizer.adjust_for_ambient_noise(source=source)
@@ -9,11 +8,9 @@ import whisper
 #    audio = recognizer.listen(source)
 #    print("done")
 
-# Guardas el audio a WAV
 #with open("grabacion.wav", "wb") as f:
 #    f.write(audio.get_wav_data())
 
-# Luego usas Whisper con ese archivo
 model = whisper.load_model("base")
 #result = model.transcribe(audio="grabacion.wav")
 result2 = model.transcribe(audio="whatsaudio1.wav")
